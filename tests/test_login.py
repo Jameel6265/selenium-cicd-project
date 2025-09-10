306c55e4a956d2b623d60487acfb36441f1e4092
Jameel6265 <zamilyaser47.ah@gmail.com>
2025-09-10 19:03:56 +0530

    Added Modified Jenkinsfile with pipeline script

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,4 +1,4 @@
-#developed by Abdul Hameed M
+#developed by Abdul Hameed M on 10/09/2025
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
